7107bb973dbeb1e9c4d5f3d6c161b0904da6f22e
Nguyen <2255146@cegep-heritage.qc.ca>
2024-10-23 12:44:54 -0400

    Extensions to batchfile

---
+++ VSCode/extensions.bat
@@ -0,0 +1,3 @@
+call C:\Users\2255146\AppData\Local\Programs\VSCodium\bin\codium --install-extension asvetliakov.vscode-neovim
+call C:\Users\2255146\AppData\Local\Programs\VSCodium\bin\codium --install-extension bprabhus.hyper-ctrl-theme
+call C:\Users\2255146\AppData\Local\Programs\VSCodium\bin\codium --install-extension yandeu.five-server

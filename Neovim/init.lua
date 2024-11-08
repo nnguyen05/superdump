072e3a279165f154cf9bd584eb753964cfbd51d5
Nguyen <2255146@cegep-heritage.qc.ca>
2024-11-08 11:18:32 -0500

    Fix newlines. I use Neovim in WSL now. It's all in WSL baby

--- Neovim/init.lua
+++ Neovim/init.lua
@@ -1,27 +1,23 @@
-local vim = vim
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+vim.g.rust_recommended_style = false
+vim.g.python_recommended_style = false
 
-local plug = vim.fn["plug#"]
-vim.call("plug#begin")
-	plug("neovim/nvim-lspconfig")
-	plug("hrsh7th/cmp-nvim-lsp")
-	plug("hrsh7th/cmp-buffer")
-	plug("hrsh7th/cmp-path")
-	plug("hrsh7th/cmp-cmdline")
-	plug("hrsh7th/nvim-cmp")
-	plug("L3MON4D3/LuaSnip")
-	plug("saadparwaiz1/cmp_luasnip")
-	
-	plug("udalov/kotlin-vim")
-vim.call("plug#end")
+vim.opt.termguicolors = true
 
 vim.cmd("silent! color vim")
 vim.cmd("silent! set nowrap")
 
+require("config.lazy")
+
 local cmp = require("cmp")
+local lspconfig = require("lspconfig")
+
 cmp.setup({
 	snippet = {
+		-- REQUIRED - you must specify a snippet engine
 		expand = function(args)
-			require('luasnip').lsp_expand(args.body)
+			require("luasnip").lsp_expand(args.body)
 		end,
 	},
 	window = {
@@ -29,28 +25,84 @@ cmp.setup({
 		-- documentation = cmp.config.window.bordered(),
 	},
 	mapping = cmp.mapping.preset.insert({
-		['<C-b>'] = cmp.mapping.scroll_docs(-4),
-		['<C-f>'] = cmp.mapping.scroll_docs(4),
-		['<C-Space>'] = cmp.mapping.complete(),
-		['<C-e>'] = cmp.mapping.abort(),
-		['<C-y>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+		["<C-b>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<C-Space>"] = cmp.mapping.complete(),
+		["<C-e>"] = cmp.mapping.abort(),
+		-- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+		["<C-y>"] = cmp.mapping.confirm({ select = true }),
 	}),
 	sources = cmp.config.sources({
-		{ name = 'nvim_lsp' },
-		{ name = 'luasnip' },
-	}, {
-		{ name = 'buffer' },
+		{ name = "nvim_lsp" },
+		{ name = "luasnip" },
+		{ name = "buffer" },
 	})
 })
 
-local lspconfig = require("lspconfig")
-lspconfig.kotlin_language_server.setup{
-	cmd = { [[C:\Users\Public\lsp\kotlin-language-server\bin\kotlin-language-server.bat]] },
-	init_options = {
-		storagePath = lspconfig.util.path.join(vim.env.XDG_DATA_HOME, "nvim-data"),
-		-- there is nothing
+cmp.setup.cmdline(":", {
+	mapping = cmp.mapping.preset.cmdline({
+		["<Up>"] = { c = cmp.mapping.select_prev_item() },
+		["<Down>"] = { c = cmp.mapping.select_next_item() },
+		["<Left>"] = { c = cmp.mapping.select_prev_item() },
+		["<Right>"] = { c = cmp.mapping.select_next_item() },
+		["<CR>"] = { c = cmp.mapping.confirm({ select = false }) },
+		["<Tab>"] = { c = cmp.mapping.confirm({ select = false }) },
+	}),
+	sources = cmp.config.sources({
+		{ name = "path" },
+		{ name = "cmdline" }
+	}),
+	matching = { disallow_symbol_nonprefix_matching = false }
+})
+
+lspconfig.rust_analyzer.setup { }
+
+lspconfig.lua_ls.setup {
+	on_init = function(client)
+		if client.workspace_folders then
+			local path = client.workspace_folders[1].name
+			if vim.uv.fs_stat(path.."/.luarc.json") or vim.uv.fs_stat(path.."/.luarc.jsonc") then
+				return
+			end
+		end
+		client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+			runtime = {
+				-- Tell the language server which version of Lua you"re using
+				-- (most likely LuaJIT in the case of Neovim)
+				version = "LuaJIT"
+			},
+			-- Make the server aware of Neovim runtime files
+			workspace = {
+				checkThirdParty = false,
+				library = {
+					vim.env.VIMRUNTIME
+					-- Depending on the usage, you might want to add additional paths here.
+					-- "${3rd}/luv/library"
+					-- "${3rd}/busted/library",
+				}
+				-- or pull in all of "runtimepath". NOTE: this is a lot slower and will cause issues when working on your own configuration (see https://github.com/neovim/nvim-lspconfig/issues/3189)
+				-- library = vim.api.nvim_get_runtime_file("", true)
+			}
+		})
+	end,
+	settings = {
+		Lua = {}
+	}
+}
+
+lspconfig.clangd.setup { }
+
+lspconfig.ts_ls.setup {
+	init_options = { hostInfo = "neovim" },
+	cmd = {"npx", "typescript-language-server", "--stdio"},
+	filetypes = {
+		"javascript",
+		"javascriptreact",
+		"javascript.jsx",
+		"typescript",
+		"typescriptreact",
+		"typescript.tsx",
 	},
-	root_dir = lspconfig.util.root_pattern(
-		"pom.xml"
-	)
+	root_dir = lspconfig.util.root_pattern("tsconfig.json", "jsconfig.json", "package.json", ".git"),
+	single_file_support = true,
 }
